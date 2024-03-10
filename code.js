48a8fcf8591948e46a597ea430204af6028158d9
Royal <royalhuseyn902@gmail.com>
2024-03-10 23:52:45 +0400

    Write test for reverseString and implement it

--- code.js
+++ code.js
@@ -2,4 +2,8 @@ function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1);
 }
 
-export { capitalize };
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+export { capitalize, reverseString };
--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from './code';
+import { capitalize, reverseString } from './code';
 
 describe('capitalize', () => {
   test('Returns string with first char capitalized', () => {
@@ -7,5 +7,19 @@ describe('capitalize', () => {
 
   test('Returns string with first char capitalized regardless of other letters', () => {
     expect(capitalize('bOOK')).toEqual('BOOK');
+  });
+});
+
+describe('reverseString', () => {
+  test('Reverses single word', () => {
+    expect(reverseString('hello')).toEqual('olleh');
+  });
+
+  test('Reverses multiple words', () => {
+    expect(reverseString('hello there')).toEqual('ereht olleh');
+  });
+
+  test('Works with punctuation', () => {
+    expect(reverseString('hell!o ? the<r>e')).toEqual('e>r<eht ? o!lleh');
   });
 });
