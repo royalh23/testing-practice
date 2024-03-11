2bc8f0eb9ebb3f6fd218abf9d5759d68558304a9
Royal <royalhuseyn902@gmail.com>
2024-03-11 10:17:49 +0400

    Write tests for calculator and implement it

--- code.js
+++ code.js
@@ -6,4 +6,13 @@ function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
-export { capitalize, reverseString };
+const calculator = (() => {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => a / b;
+
+  return { add, subtract, multiply, divide };
+})();
+
+export { capitalize, reverseString, calculator };
--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './code';
+import { capitalize, reverseString, calculator } from './code';
 
 describe('capitalize', () => {
   test('Returns string with first char capitalized', () => {
@@ -21,5 +21,39 @@ describe('reverseString', () => {
 
   test('Works with punctuation', () => {
     expect(reverseString('hell!o ? the<r>e')).toEqual('e>r<eht ? o!lleh');
+  });
+});
+
+describe('calculator', () => {
+  test('Adds small numbers', () => {
+    expect(calculator.add(2, 1)).toEqual(3);
+  });
+
+  test('Adds large numbers', () => {
+    expect(calculator.add(1095, 2036)).toEqual(3131);
+  });
+
+  test('Subtracts small numbers', () => {
+    expect(calculator.subtract(2, 1)).toEqual(1);
+  });
+
+  test('Subtracts large numbers', () => {
+    expect(calculator.subtract(2365, 4523)).toEqual(-2158);
+  });
+
+  test('Multiplies small numbers', () => {
+    expect(calculator.multiply(2, 3)).toEqual(6);
+  });
+
+  test('Multiplies large numbers', () => {
+    expect(calculator.multiply(2365, 4523)).toEqual(10696895);
+  });
+
+  test('Divides small numbers', () => {
+    expect(calculator.divide(6, 2)).toEqual(3);
+  });
+
+  test('Divides large numbers', () => {
+    expect(calculator.divide(1000, 250)).toEqual(4);
   });
 });
